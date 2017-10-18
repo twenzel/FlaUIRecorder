e25d22ac3cffe066bd84b46df45ec24d7f3c294e
André Raabe <andre.raabe@gmail.com>
2017-10-18 18:32:23 +0200

    Add PowerShell code provider

---
+++ src/FlaUIRecorder.CodeProvider.PowerShell/PowerShellCodeHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+using FlaUI.Core.AutomationElements.Infrastructure;
+
+namespace FlaUIRecorder.CodeProvider.PowerShell
+{
+    public static class PowerShellCodeHelper
+    {
+        public static string GetVariableName(this AutomationElement element)
+        {
+            if (null == element)
+                throw new ArgumentNullException(nameof(element));
+
+            string controlTypeName;
+
+            if (element.Properties.Name.TryGetValue(out var elementName) && !string.IsNullOrEmpty(elementName))
+                elementName = Char.ToLower(elementName[0]) + elementName.Substring(1); // to camel case
+            else
+                elementName = "unkown";
+
+            elementName = Regex.Replace(elementName, "[^\\d\\w_]", "");
+
+            if (element.Properties.ControlType.TryGetValue(out var controlType))
+                controlTypeName = controlType.ToString();
+            else
+                controlTypeName = "Item";
+
+            return elementName + controlTypeName;
+        }
+    }
+}
